abd6eb421379e4b2a1b8e88d9f88d01d68b21c22
YonatanTal <yonatald@gmail.com>
2020-12-06 09:19:31 +0200

    fix: minor fixes
    Signed-off-by: YonatanTal <yonatald@gmail.com>

--- src/utils/errors/server.error.ts
+++ src/utils/errors/server.error.ts
@@ -1,19 +1,7 @@
 import { ServerError } from './application.error';
 
-export class SpikeError extends ServerError {
-  constructor(message?: string) {
-    super(message || 'Error contacting spike');
-  }
-}
-
 export class KartoffelError extends ServerError {
   constructor(message?: string) {
     super(message || 'Error contacting kartoffel');
-  }
-}
-
-export class ApprovalError extends ServerError {
-  constructor(message?: string) {
-    super(message || 'Error contacting approval service');
   }
 }
